ca6a3635ef5ed76a302a9e60d9797949cb2620db
Maxim Utkin <maxim2001_2001@mail.ru>
2021-06-14 23:22:47 +0300

    Просто проба

--- src/Main/main.js
+++ src/Main/main.js
@@ -5,7 +5,7 @@ export default()=>{
         <main>
             
             <h1 style={{fontSize:"calc( (100vw - 480px)/(1280 - 480) * (24 - 16) + 16px);", color:"#fff", position:"absolute", top:"50%", left:"50%", transform:"translate(-50%)"}}>Приветствуем Вас!</h1>
-        
+            <h2></h2>
         </main>
         
     )
